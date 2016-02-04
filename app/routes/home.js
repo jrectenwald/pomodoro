75db6345880294da6a57585ee68b278a29ccd6f5
jrectenwald <jimrec101@gmail.com>
2016-02-04 10:20:42 -0500

    user delete function

--- app/routes/home.js
+++ app/routes/home.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findAll('user');
+  },
+  actions: {
+    delete(friend) { 
+      friend.destroyRecord().then(() => {
+        this.transitionTo('home'); 
+      });
+    }
   }
 });
