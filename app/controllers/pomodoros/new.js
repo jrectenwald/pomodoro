13e7ce675789d1086de01cd183e5cc2972dac740
jrectenwald <jimrec101@gmail.com>
2016-02-04 12:20:56 -0500

    adds new pomodoro

---
+++ app/controllers/pomodoros/new.js
@@ -0,0 +1,26 @@
+export default Ember.Controller.extend({ 
+  isValid: Ember.computed(
+    'model.task',
+    {
+      get() {
+        return !Ember.isEmpty(this.get('model.task')) 
+      }
+    }), 
+
+  actions: {
+    save() {
+      if (this.get('isValid')) { 
+        this.get('model').save().then((pomodoro) => {
+          this.transitionToRoute('pomodoros.pomodoro', pomodoro); 
+        });
+      } else {
+        this.set('errorMessage', 'You have to fill all the fields');
+      }
+      return false; 
+    },
+    cancel() { 
+      this.transitionToRoute('home');
+      return false; 
+    }
+  }
+});
